b45a33898f456a5dae7cf9dcc05831e95dd46d32
genghiskhang <khang11475@gmail.com>
2021-12-01 07:46:06 -0500

    2021 day 1 DONE

---
+++ 2021/2021day1.py
@@ -0,0 +1,21 @@
+import re
+import os
+import sys
+
+dataset = []
+with open(os.path.join(sys.path[0], "2021day1input.txt")) as f:
+    dataset = list(map(int, f.read().split('\n')))
+    
+def part1(input):
+    increase = 0
+    for i in range(1, len(input)):
+        if input[i] > input[i - 1]:
+            increase += 1
+    return increase
+
+def part2(input):
+    increase = 0
+    for i in range(3, len(input)):
+        if input[i] + input[i - 1] + input[i - 2] > input[i - 1] + input[i - 2] + input[i - 3]:
+            increase += 1
+    return increase
